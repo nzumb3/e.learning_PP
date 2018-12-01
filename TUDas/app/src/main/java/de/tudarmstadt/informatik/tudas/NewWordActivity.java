db70eb56ce77a87b568fce4bf88eed3f45820833
Bene <Benedikt_Lins@gmx.de>
2018-12-01 14:14:04 +0100

    Add appointment view

--- TUDas/app/src/main/java/de/tudarmstadt/informatik/tudas/NewWordActivity.java
+++ TUDas/app/src/main/java/de/tudarmstadt/informatik/tudas/NewWordActivity.java
@@ -1,18 +1,33 @@
 package de.tudarmstadt.informatik.tudas;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class NewWordActivity extends AppCompatActivity {
 
     public static final String EXTRA_REPLY = "com.example.android.wordlistsql.REPLY";
 
     private EditText mEditWordView;
+
+    private Calendar myCalendar = Calendar.getInstance();
+    private EditText edittext;
+
+    private void updateLabel() {
+        String myFormat = "dd/MM/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.GERMANY);
+        edittext.setText(sdf.format(myCalendar.getTime()));
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,5 +49,23 @@ public class NewWordActivity extends AppCompatActivity {
                 finish();
             }
         });
+        this.edittext = (EditText) findViewById(R.id.date_input);
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+        edittext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(NewWordActivity.this, date, myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
     }
 }
