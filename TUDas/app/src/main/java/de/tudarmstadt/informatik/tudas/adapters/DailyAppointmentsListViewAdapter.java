077812e164cdfa170ee7ac77becc2a4fdc14ffd8
Bene <Benedikt_Lins@gmx.de>
2019-01-18 14:57:07 +0100

    DailyAppointments adapter

--- TUDas/app/src/main/java/de/tudarmstadt/informatik/tudas/adapters/DailyAppointmentsListViewAdapter.java
+++ TUDas/app/src/main/java/de/tudarmstadt/informatik/tudas/adapters/DailyAppointmentsListViewAdapter.java
@@ -1,6 +1,7 @@
 package de.tudarmstadt.informatik.tudas.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,33 +14,20 @@ import java.util.List;
 
 import de.tudarmstadt.informatik.tudas.R;
 import de.tudarmstadt.informatik.tudas.model.Appointment;
+import de.tudarmstadt.informatik.tudas.viewmodels.TimeTableViewModel;
 
-public class DailyAppointmentsListViewAdapter extends BaseAdapter {
+public class DailyAppointmentsListViewAdapter extends AbstractListAdapter<Appointment> {
 
     private LayoutInflater layoutInflater;
     private List<Appointment> appointments;
 
-    public DailyAppointmentsListViewAdapter(Context context) {
-        layoutInflater = LayoutInflater.from(context);
+    public DailyAppointmentsListViewAdapter(Context context, List<Appointment> appointments) {
+        super(context);
+        this.appointments = appointments;
     }
 
     private void setAppointments(List<Appointment> appointments){
         this.appointments = appointments;
-    }
-
-    @Override
-    public int getCount() {
-        return appointments.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return appointments.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
     }
 
     @Override
@@ -48,8 +36,25 @@ public class DailyAppointmentsListViewAdapter extends BaseAdapter {
             convertView = layoutInflater.inflate(R.layout.component_daily_appointments_item, null);
 
         if (appointments != null && appointments.size() >= position + 1){
+            Appointment appointment = list.get(position);
             RelativeLayout entry = convertView.findViewById(R.id.rlDailyAppointment);
-            //TODO
+            TextView title = convertView.findViewById(R.id.dailyAppointmentTitle);
+            TextView abbr = convertView.findViewById(R.id.dailyAppointmentAbbrevation);
+            TextView room = convertView.findViewById(R.id.dailyAppointmentRoom);
+            TextView start = convertView.findViewById(R.id.tvAppointmentStartTime);
+            TextView end = convertView.findViewById(R.id.tvAppointmentEndTime);
+            entry.setBackgroundColor(Color.parseColor(appointment.getAppointmentContent().getColor()));
+            int color = Color.parseColor(TimeTableViewModel.getComplementaryColor(appointment.getAppointmentContent().getColor()));
+            title.setText(appointment.getAppointmentContent().getTitle());
+            title.setTextColor(color);
+            abbr.setText(appointment.getAppointmentContent().getAbbreviation());
+            abbr.setTextColor(color);
+            room.setText(appointment.getAppointmentContent().getRoom());
+            room.setTextColor(color);
+            start.setText(appointment.getStartDate().getTime().toString());
+            start.setTextColor(color);
+            end.setText(appointment.getEndDate().getTime().toString());
+            end.setTextColor(color);
         }
         return convertView;
     }
