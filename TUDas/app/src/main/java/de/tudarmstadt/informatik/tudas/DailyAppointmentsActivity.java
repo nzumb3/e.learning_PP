3c4ff8ab0c683f4fbba2534d89594827db3d9029
Bene <Benedikt_Lins@gmx.de>
2019-03-19 15:02:04 +0100

    added navigation to timetable

--- TUDas/app/src/main/java/de/tudarmstadt/informatik/tudas/DailyAppointmentsActivity.java
+++ TUDas/app/src/main/java/de/tudarmstadt/informatik/tudas/DailyAppointmentsActivity.java
@@ -9,17 +9,9 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.PopupWindow;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import java.util.Calendar;
 import java.util.Locale;
