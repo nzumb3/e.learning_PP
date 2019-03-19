0311dec2cde6c02471ceae3577c9e9337b6a51fd
Bene <Benedikt_Lins@gmx.de>
2019-03-19 13:58:54 +0100

    Added functionality for entering a room and also a popup to show after hitting save button

--- TUDas/app/src/main/java/de/tudarmstadt/informatik/tudas/NewAppointmentActivity.java
+++ TUDas/app/src/main/java/de/tudarmstadt/informatik/tudas/NewAppointmentActivity.java
@@ -7,6 +7,7 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -51,6 +52,7 @@ public class NewAppointmentActivity extends AppCompatActivity {
     private EditText end_time_input;
     private EditText abbrInput;
     private EditText roomInput;
+    private Snackbar saveBar;
 
     private boolean hour24Format;
 
@@ -104,7 +106,10 @@ public class NewAppointmentActivity extends AppCompatActivity {
         mEditWordView = findViewById(R.id.appointment_title_input);
         descriptionInput = findViewById(R.id.appointment_description_input);
         abbrInput = findViewById(R.id.appointment_abbrevation_input);
-        //roomInput = findViewById("S202/C120");
+        roomInput = findViewById(R.id.appointment_room_input);
+
+        //Snackbar for showing save information
+        saveBar = Snackbar.make(findViewById(R.id.new_appointment_layout), R.string.appointment_save, Snackbar.LENGTH_SHORT);
 
         //COLOR CHANGER
         colorPreview = findViewById(R.id.appointment_color_preview);
@@ -125,12 +130,12 @@ public class NewAppointmentActivity extends AppCompatActivity {
                 int background = ((ColorDrawable) findViewById(R.id.appointment_color_preview).getBackground()).getColor();
                 String colorStr = "#" + Integer.toHexString(background & 0x00ffffff);
                 content.setColor(colorStr);
-                //TODO!!!
-                content.setRoom("S202/C120");
+                content.setRoom(roomInput.getText().toString());
                 Appointment app = new Appointment();
                 app.setStartDate(start_date_calendar);
                 app.setEndDate(end_date_calendar);
                 viewModel.insert(content, app);
+                saveBar.show();
                 /*
                 Intent replyIntent = new Intent();
                 if (TextUtils.isEmpty(mEditWordView.getText())) {
@@ -154,40 +159,10 @@ public class NewAppointmentActivity extends AppCompatActivity {
             }
         });
         start_date_input = (EditText) findViewById(R.id.start_date_input);
-        DatePickerDialog.OnDateSetListener date_start = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                start_date_calendar.set(Calendar.YEAR, year);
-                start_date_calendar.set(Calendar.MONTH, month);
-                start_date_calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                updateLabel();
-            }
-        };
-        start_date_input.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new DatePickerDialog(NewAppointmentActivity.this, date_start, start_date_calendar.get(Calendar.YEAR),
-                        start_date_calendar.get(Calendar.MONTH), start_date_calendar.get(Calendar.DAY_OF_MONTH)).show();
-            }
-        });
-        end_date_input = (EditText) findViewById(R.id.end_date_input);
-        DatePickerDialog.OnDateSetListener date_end = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                end_date_calendar.set(Calendar.YEAR, year);
-                end_date_calendar.set(Calendar.MONTH, month);
-                end_date_calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                updateLabel();
-            }
-        };
-        end_date_input.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new DatePickerDialog(NewAppointmentActivity.this, date_end, end_date_calendar.get(Calendar.YEAR),
-                        end_date_calendar.get(Calendar.MONTH), end_date_calendar.get(Calendar.DAY_OF_MONTH)).show();
-            }
-        });
         start_time_input = (EditText) findViewById(R.id.start_time_input);
+        end_date_input = (EditText) findViewById(R.id.end_date_input);
+        end_time_input = (EditText) findViewById(R.id.end_time_input);
+
         TimePickerDialog.OnTimeSetListener time_start = new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
@@ -202,7 +177,6 @@ public class NewAppointmentActivity extends AppCompatActivity {
                 new TimePickerDialog(NewAppointmentActivity.this, time_start, start_date_calendar.get(Calendar.HOUR_OF_DAY), start_date_calendar.get(Calendar.MINUTE), hour24Format).show();
             }
         });
-        this.end_time_input = (EditText) findViewById(R.id.end_time_input);
         TimePickerDialog.OnTimeSetListener time_end = new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
@@ -215,6 +189,41 @@ public class NewAppointmentActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 new TimePickerDialog(NewAppointmentActivity.this, time_end, end_date_calendar.get(Calendar.HOUR_OF_DAY), end_date_calendar.get(Calendar.MINUTE), hour24Format).show();
+            }
+        });
+
+        DatePickerDialog.OnDateSetListener date_start = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                start_date_calendar.set(Calendar.YEAR, year);
+                start_date_calendar.set(Calendar.MONTH, month);
+                start_date_calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+                new TimePickerDialog(NewAppointmentActivity.this, time_start, start_date_calendar.get(Calendar.HOUR_OF_DAY), start_date_calendar.get(Calendar.MINUTE), hour24Format).show();
+            }
+        };
+        start_date_input.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(NewAppointmentActivity.this, date_start, start_date_calendar.get(Calendar.YEAR),
+                        start_date_calendar.get(Calendar.MONTH), start_date_calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+        DatePickerDialog.OnDateSetListener date_end = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                end_date_calendar.set(Calendar.YEAR, year);
+                end_date_calendar.set(Calendar.MONTH, month);
+                end_date_calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+                new TimePickerDialog(NewAppointmentActivity.this, time_end, end_date_calendar.get(Calendar.HOUR_OF_DAY), end_date_calendar.get(Calendar.MINUTE), hour24Format).show();
+            }
+        };
+        end_date_input.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(NewAppointmentActivity.this, date_end, end_date_calendar.get(Calendar.YEAR),
+                        end_date_calendar.get(Calendar.MONTH), end_date_calendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
         updateLabel();
